60379e0fdf2e5436b2cb9ef0b230d9abb72a12a4
Tal Israel <ty4273@bs.amalnet.k12.il>
2020-02-16 21:13:37 +0200

    Login and register

--- app/src/main/java/com/tal/beta/MainActivity.java
+++ app/src/main/java/com/tal/beta/MainActivity.java
@@ -2,6 +2,7 @@ package com.tal.beta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent t = new Intent(this, LoginActivity.class);
+        startActivity(t);
     }
 }
